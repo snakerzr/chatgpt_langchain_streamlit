989bc08b4ee6fd59a232e56c3acf8ae32523f968
Alexander Evdokimov <83905079+snakerzr@users.noreply.github.com>
2023-08-18 21:25:22 +0300

    fix: streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -8,22 +8,26 @@ from langchain.agents import AgentType
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import StreamlitCallbackHandler
 
-open_ai_api = st.text_input('OpenAI API:')
-os.environ["OPENAI_API_KEY"] = open_ai_api
+st.caption('Enter OpenAI API key')
+open_ai_api = st.text_input('OpenAI API:',key='open_ai_key')
 
-llm = ChatOpenAI(temperature=0)
-tools = load_tools(["wikipedia"], llm=llm)
+if st.session_state['open_ai_key'] != '':
+    os.environ["OPENAI_API_KEY"] = open_ai_api
 
-agent= initialize_agent(
-    tools, 
-    llm, 
-    agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-    handle_parsing_errors=True,
-    verbose = False)
+    llm = ChatOpenAI(temperature=0)
+    tools = load_tools(["wikipedia"], llm=llm)
 
-if prompt := st.chat_input():
-    st.chat_message("user").write(prompt)
-    with st.chat_message("assistant"):
-        st_callback = StreamlitCallbackHandler(st.container())
-        response = agent.run(prompt, callbacks=[st_callback])
-        st.write(response)
+    agent= initialize_agent(
+        tools, 
+        llm, 
+        agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+        handle_parsing_errors=True,
+        verbose = False)
+    st.caption('Ask questions, ignore parsing errors:')
+
+    if prompt := st.chat_input():
+        st.chat_message("user").write(prompt)
+        with st.chat_message("assistant"):
+            st_callback = StreamlitCallbackHandler(st.container())
+            response = agent.run(prompt, callbacks=[st_callback])
+            st.write(response)
